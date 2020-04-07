c8e2288f0edc977c43eec602edf8065e3aa7e077
Michael Equi <equim20@student.jhs.net>
2020-04-07 14:51:32 -0700

    updates to bytes-orb-slam repo

--- include/br-slam/br-slam.h
+++ include/br-slam/br-slam.h
@@ -12,8 +12,17 @@
 
 #include <message_filters/subscriber.h>
 #include <message_filters/time_synchronizer.h>
+#include <message_filters/sync_policies/approximate_time.h>
+
 #include <sensor_msgs/msg/image.hpp>
+#include <sensor_msgs/msg/imu.hpp>
 #include <sensor_msgs/msg/camera_info.hpp>
+#include <nav_msgs/msg/odometry.hpp>
+#include <geometry_msgs/msg/pose_with_covariance_stamped.hpp>
+
+#include <cv_bridge/cv_bridge.h>
+
+#include "System.h"
 
 
 class BytesSlam: public rclcpp::Node{
@@ -22,9 +31,35 @@ public:
 private:
     // Handle subscribing to stereo image topics
     message_filters::Subscriber<sensor_msgs::msg::Image> left_image_sub_;
+    message_filters::Subscriber<sensor_msgs::msg::CameraInfo> left_image_info_sub_;
     message_filters::Subscriber<sensor_msgs::msg::Image> right_image_sub_;
-    message_filters::TimeSynchronizer<sensor_msgs::msg::Image, sensor_msgs::msg::Image> sync_;
-    void stereo_cb(const std::shared_ptr<sensor_msgs::msg::Image>& left, const std::shared_ptr<sensor_msgs::msg::Image>& right);
+    message_filters::Subscriber<sensor_msgs::msg::CameraInfo> right_image_info_sub_;
+
+    rclcpp::Subscription<sensor_msgs::msg::Imu>::SharedPtr imu_sub;
+    rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr wheel_odom_sub;
+    rclcpp::Subscription<geometry_msgs::msg::PoseWithCovarianceStamped>::SharedPtr altimeter_sub;
+
+    // The follow is synchronized with images to provide optimal SLAM. Images should be publishing at 15fps
+    message_filters::TimeSynchronizer<sensor_msgs::msg::Image, sensor_msgs::msg::CameraInfo,
+                                      sensor_msgs::msg::Image, sensor_msgs::msg::CameraInfo> sync_;
+
+    void stereo_cb(const std::shared_ptr<sensor_msgs::msg::Image>& left, const std::shared_ptr<sensor_msgs::msg::CameraInfo>& left_info,
+                   const std::shared_ptr<sensor_msgs::msg::Image>& right, const std::shared_ptr<sensor_msgs::msg::CameraInfo>& right_info);
+
+    // CV bridge for converting ROS image topics to Opencv topics
+    cv_bridge::CvImageConstPtr right_cv_ptr;
+    cv_bridge::CvImageConstPtr left_cv_ptr;
+
+    //Standard callbacks for IMU, wheel odom, altimeter @ 30Hz
+    void imu_cb(const sensor_msgs::msg::Imu::SharedPtr data);
+    sensor_msgs::msg::Imu::SharedPtr imu_data;
+    void wheel_odom_cb(const nav_msgs::msg::Odometry::SharedPtr data);
+    nav_msgs::msg::Odometry::SharedPtr wheel_odom_data;
+    void altimeter_cb(const geometry_msgs::msg::PoseWithCovarianceStamped::SharedPtr data);
+    geometry_msgs::msg::PoseWithCovarianceStamped::SharedPtr altimeter_data;
+
+    // Bytes ORB SLAM system
+    System slam;
 };
 
 #endif // BR_SLAM_BR_SLAM_H
--- src/br-slam.cpp
+++ src/br-slam.cpp
@@ -3,16 +3,49 @@
 // Constructor for the Bytes SLAM node
 BytesSlam::BytesSlam() : Node("bytes_slam"),
 left_image_sub_(this, "stereo/left/resize/image"),
+left_image_info_sub_(this, "stereo/left/resize/camera_info"),
 right_image_sub_(this, "stereo/right/resize/image"),
-sync_(left_image_sub_, right_image_sub_, 10)
+right_image_info_sub_(this, "stereo/right/resize/camera_info"),
+sync_(left_image_sub_, left_image_info_sub_, right_image_sub_, right_image_info_sub_, 10),
+slam(dynamic_cast<Node*>(this), "/home/michael")
 {
+    RCLCPP_INFO(this->get_logger(), "Beginning Bytes SLAM");
     sync_.registerCallback(&BytesSlam::stereo_cb, this);
+    imu_sub = this->create_subscription<sensor_msgs::msg::Imu>("imu/data", 10, std::bind(&BytesSlam::imu_cb, this, std::placeholders::_1));
+    wheel_odom_sub = this->create_subscription<nav_msgs::msg::Odometry>("mobile_base_controller/odom", 10, std::bind(&BytesSlam::wheel_odom_cb, this, std::placeholders::_1));
+    altimeter_sub = this->create_subscription<geometry_msgs::msg::PoseWithCovarianceStamped>("altimeter/pose", 10, std::bind(&BytesSlam::altimeter_cb, this, std::placeholders::_1));
 }
 
 // Callback for the stereo camera pair called on synchronization with left and right images
-void BytesSlam::stereo_cb(const std::shared_ptr<sensor_msgs::msg::Image>& left, const std::shared_ptr<sensor_msgs::msg::Image>& right) {
-//    RCLCPP_INFO(this->get_logger(), left->header.frame_id.c_str());
-//    RCLCPP_INFO(this->get_logger(), right->header.frame_id.c_str());
+void BytesSlam::stereo_cb(const std::shared_ptr<sensor_msgs::msg::Image>& left, const std::shared_ptr<sensor_msgs::msg::CameraInfo>& left_info,
+                          const std::shared_ptr<sensor_msgs::msg::Image>& right, const std::shared_ptr<sensor_msgs::msg::CameraInfo>& right_info) {
+    try {
+        left_cv_ptr = cv_bridge::toCvShare(left, "mono8");
+    } catch (cv_bridge::Exception& e) {
+        RCLCPP_ERROR(this->get_logger(), "cv_bridge exception: %s", e.what());
+        return;
+    }
+
+    try {
+        right_cv_ptr = cv_bridge::toCvShare(right, "mono8");
+    } catch (cv_bridge::Exception& e) {
+        RCLCPP_ERROR(this->get_logger(), "cv_bridge exception: %s", e.what());
+        return;
+    }
+
+    slam.Track(left_cv_ptr->image, right_cv_ptr->image, (static_cast<double>(left_info->header.stamp.sec) + static_cast<double>(left_info->header.stamp.nanosec)/1e9));
+}
+
+void BytesSlam::imu_cb(const sensor_msgs::msg::Imu::SharedPtr data) {
+    imu_data = data;
+}
+
+void BytesSlam::wheel_odom_cb(const nav_msgs::msg::Odometry::SharedPtr data) {
+    wheel_odom_data = data;
+}
+
+void BytesSlam::altimeter_cb(const geometry_msgs::msg::PoseWithCovarianceStamped::SharedPtr data) {
+    altimeter_data = data;
 }
 
 int main(int argc, char ** argv)
